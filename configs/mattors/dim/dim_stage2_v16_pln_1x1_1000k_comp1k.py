91b2606da66efed1fe90feef5bcf0957179546f0
wangruohui <12756472+wangruohui@users.noreply.github.com>
2022-06-14 14:19:51 +0800

    update dim config

--- configs/mattors/dim/dim_stage2_v16_pln_1x1_1000k_comp1k.py
+++ configs/mattors/dim/dim_stage2_v16_pln_1x1_1000k_comp1k.py
@@ -6,7 +6,7 @@ model = dict(
     pretrained=None,  # make sure to set load_from to dim stage 1
     loss_refine=dict(type='CharbonnierLoss'),
     train_cfg=dict(train_backbone=False, train_refiner=True),
-    test_cfg=dict(refine=True, ),
+    test_cfg=dict(refine=True),
 )
 
 load_from = './checkpoints/dim_stage1_v16_1x1_1000k_comp1k_SAD-53.8_20200605_140257-979a420f.pth'
--- configs/mattors/dim/dim_stage3_v16_pln_1x1_1000k_comp1k.py
+++ configs/mattors/dim/dim_stage3_v16_pln_1x1_1000k_comp1k.py
@@ -1,4 +1,4 @@
-_base_ = ['dim_base.py']
+_base_ = ['dim_stage1_v16_1x1_1000k_comp1k.py']
 
 # model settings
 model = dict(
@@ -6,6 +6,6 @@ model = dict(
     pretrained=None,  # make sure to set load_from to dim stage 2
     loss_refine=dict(type='CharbonnierLoss'),
     train_cfg=dict(train_backbone=True, train_refiner=True),
-    test_cfg=dict(refine=True, ))
+    test_cfg=dict(refine=True))
 
 load_from = './checkpoints/dim_stage2_v16_pln_1x1_1000k_comp1k_SAD-52.3_20200607_171909-d83c4775.pth'
